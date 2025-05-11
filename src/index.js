fb102a728a395bd110c9a3050f8aa74af7290c1a
Danilo Santana <108311986+DSantana04@users.noreply.github.com>
2025-05-11 23:28:47 +0000

    feat: Adding CRUD to the application

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from './database/configdb.js';
-import exampleRoute from './routes/example.route.js';
 
+import exampleRoute from './routes/example.route.js';
 import userRoute from './routes/user.route.js';
+import workoutRoute from './routes/workout.route.js';
 
 dotenv.config();
-
-console.log(process.env.JWT_SECRET)
-
 db.connect();
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/secureExampleRoute", exampleRoute);
+app.use("/workout", workoutRoute);
 
 app.get('/', (req, res) => {
     res.send({message: 'API funcionando!'});
